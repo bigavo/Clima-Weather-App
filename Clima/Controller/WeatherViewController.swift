c06d945de1d186161782db7171435b089e76079a
Trinh Tran <kieutrinh.ibc@gmail.com>
2022-09-10 15:42:52 +0300

    Add authorizationStatus() function and didChangeAuthorization function

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -22,14 +22,44 @@ class WeatherViewController: UIViewController{
         
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
+//        locationManager.startUpdatingLocation()
+        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse || CLLocationManager.authorizationStatus() == .authorizedAlways){
+//                locationManager.requestLocation()
+            locationManager.startUpdatingLocation()
+            // Higher accuracy will require longer time to retrieve location data
+            locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+            }
         
         searchTextField.delegate = self
         weatherManager.delegate = self
     }
     
     @IBAction func locationPress(_ sender: Any) {
-        locationManager.startUpdatingLocation()
+//        locationManager.startUpdatingLocation()
+        let status = CLLocationManager.authorizationStatus()
+
+            if(status == .denied || status == .restricted || !CLLocationManager.locationServicesEnabled()){
+                // show alert to user telling them they need to allow location data to use some feature of your app
+                
+                print("show alert to user telling them they need to allow location data to use some feature of your app")
+                return
+            }
+
+            // if haven't show location permission dialog before, show it to user
+            if(status == .notDetermined){
+                locationManager.requestWhenInUseAuthorization()
+
+                // if you want the app to retrieve location data even in background, use requestAlwaysAuthorization
+                // locationManager.requestAlwaysAuthorization()
+                return
+            }
+            
+            // at this point the authorization status is authorized
+            // request location data once
+            locationManager.startUpdatingLocation()
+          
+            // start monitoring location data and get notified whenever there is change in location data / every few seconds, until stopUpdatingLocation() is called
+            // locationManager.startUpdatingLocation()
     }
     
 }
@@ -71,7 +101,10 @@ extension WeatherViewController: WeatherManagerDelegate {
     }
     
     func didFailWithError(error: Error) {
-        print(error)
+        // might be that user didn't enable location service on the device
+            // or there might be no GPS signal inside a building
+          
+            // might be a good idea to show an alert to user to ask them to walk to a place with GPS signal
     }
     
 }
@@ -91,7 +124,26 @@ extension WeatherViewController: CLLocationManagerDelegate {
             let lon = location.coordinate.longitude
             weatherManager.fetchWeather(latitude: lat, longitude: lon)
         }
+        
     }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            print("location manager authorization status changed")
+        switch status {
+        case .authorizedAlways:
+            print("user allow app to get location data when app is active or in background")
+        case .authorizedWhenInUse:
+            print("user allow app to get location data only when app is active")
+        case .denied:
+            print("user tap 'disallow' on the permission dialog, cant get location data")
+        case .restricted:
+            print("parental control setting disallow location data")
+        case .notDetermined:
+            print("the location permission dialog haven't shown before, user haven't tap allow/disallow")
+        }
+    }
+    
+    
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         if let clErr = error as? CLError {
